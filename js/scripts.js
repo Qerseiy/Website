3134b9c34977f8461e64cd2fb151e5b00c6f327b
qerseiy <qerseiy@gmail.com>
2022-10-08 15:15:05 -0700

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+let diffElem = document.querySelector("#dayDiff");
+
+const diffDays = (date, otherDate) =>
+  Math.ceil(Math.abs(date - otherDate) / (1000 * 60 * 60 * 24));
+
+diffElem.innerText = diffDays(new Date(), new Date("2023-08-06"));
+
+let speakerElem = document.querySelector("#speaker");
+let isSpeakerPlaying = false;
+let music = new Audio("assets/my_part_song1.m4r");
+music.loop = true;
+
+speakerElem.addEventListener("click", function () {
+  if (!isSpeakerPlaying) {
+    speakerElem.src = "assets/speaker-playing.svg";
+    isSpeakerPlaying = true;
+    music.play();
+  } else {
+    speakerElem.src = "assets/speaker-muted.svg";
+    isSpeakerPlaying = false;
+    music.pause();
+    music.currentTime = 0;
+  }
+});
