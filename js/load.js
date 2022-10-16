aa2e33dbc69a24763844a28c8cc429e394a3d105
qerseiy <qerseiy@gmail.com>
2022-10-16 11:41:16 -0700

    New music

--- js/load.js
+++ js/load.js
@@ -1,6 +1,5 @@
 window.onload = async function () {
   document.getElementById("main-loading-overlay").style.opacity = "0";
   document.getElementById("main-loading-overlay").style.visibility = "hidden";
-
   document.getElementById('centerScroll').scrollIntoView();
 };
